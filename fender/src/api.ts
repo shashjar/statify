265e37995a2714345e3c98dd04141e053cd5601f
Shashank Jarmale <shashankjarmale1@gmail.com>
2024-08-02 23:45:44 -0700

    Separated API in fender into default and streaming (Axios)

--- fender/src/api.ts
+++ fender/src/api.ts
@@ -2,8 +2,13 @@ import axios from "axios";
 
 const apiUrl = "/choreo-apis/statify/app/v1";
 
-const api = axios.create({
+const defaultAPI = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
-export default api;
+const streamingAPI = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  responseType: "stream",
+});
+
+export { defaultAPI, streamingAPI };
